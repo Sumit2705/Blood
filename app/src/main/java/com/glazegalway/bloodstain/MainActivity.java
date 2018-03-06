042507924548f721eb9c582757118fa03cdc6f7b
Sumit2705 <enggsumi.05@gmail.com>
2018-03-06 13:39:07 +0530

    test

--- app/src/main/java/com/glazegalway/bloodstain/MainActivity.java
+++ app/src/main/java/com/glazegalway/bloodstain/MainActivity.java
@@ -2,20 +2,18 @@ package com.glazegalway.bloodstain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     
-    String st_name = "Sumit"
+    String st_name = "Sumit 2705";
     TextView tv_textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        tv_textview = (TextView) findviewbyid(R.id.tv_textview);
-        tv_textview.setText(st_name);
-          
+
         
         
     }
